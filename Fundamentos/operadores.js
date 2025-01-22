d91dfd38345560002d3fa58f6bcf55d048bc2dce
Thiago Sanches <thiagomlkgame@gmail.com>
2025-01-22 17:14:17 -0300

    atualização de documentos js aula 2.0

---
+++ Fundamentos/operadores.js
@@ -0,0 +1,37 @@
+/**
+ * Operadores
+ * - Aritméticos
+ * - Aritéticos de Atribuição 
+ * - Comparação
+ * - Logicos
+ */
+
+console.clear()
+console.log("operadores aritiméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("+ | modulo(resto da divisão)")
+console.log("")
+console.log("Operadores aritméticos de aribuação")
+console.log("+= | soma e atribuação a variavel")
+console.log("-= | subtração e atribuição a variavel")
+console.log("*= | multiplicação e atribuição a variavel")
+console.log("/= | divisão e atribuição a variavel")
+console.log("++ | somar 1 ao resultado")
+console.log("-- | subtrair 1 do resultado ")
+console.log("")
+console.log("comparação")
+console.log("> | maior que ")
+console.log("< | menor que ")
+console.log(">= | manor ou igual a ")
+console.log("<= | menor ou igual a ")
+console.log("=== | identico a ")
+console.log("!== | não identico a ")
+
+
+
+
+
+
